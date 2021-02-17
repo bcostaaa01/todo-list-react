6a80b7c7a4860ebeb62b431dc354830e23c5ad2a
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2021-02-17 13:22:35 +0000

    Add state to SubmitForm Add state
    (When state is changed, React controls the value of the input field)
    
    Add onChange handler
    
    (this will update the state when we type anything in the input)

--- src/submitform.js
+++ src/submitform.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
 
 class SubmitForm extends Component {
+  state = { term: "" };
+
   render() {
     return (
       <form>
-        <input type="text" className="input" placeholder="Enter Todo Task" />
+        <input
+          type="text"
+          className="input"
+          placeholder="Enter Todo Task"
+          value={this.state.term}
+          onChange={(e) =>
+            this.setState({
+              term: e.target.value
+            })
+          }
+        />
         <button className="button">Submit Todo task</button>
       </form>
     );
