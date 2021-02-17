a97f50ae92487c3df2d0c14692df36aae3274526
Bruno <72168158+bcostaaa01@users.noreply.github.com>
2021-02-17 14:56:35 +0000

    Update todo.js

--- src/todo.js
+++ src/todo.js
@@ -4,14 +4,14 @@ const Todo = (props) => {
   return (
     <div className="list-item">
       {props.content}
-      // delete button
+      
       <button
         class="delete is-pulled-right"
         onClick={() => {
           props.onDelete(props.id);
         }}
       ></button>
-      // rename button
+      
       <button class="rename"
       onClick={() => {
         props.onChange(props.id);
